b63c7d174eb5a7a4383c3a48041c7ba7d62f348e
Suparna_0 <119069791+Suparnauchiha@users.noreply.github.com>
2024-08-04 18:28:50 +0530

    Create multiply_two_lists.cpp

---
+++ multiply_two_lists.cpp
@@ -0,0 +1,14 @@
+long long  multiplyTwoLists (Node* l1, Node* l2)
+{
+  long long num1 = 0;
+  long long num2 = 0;
+  while(l1!=nullptr){
+      num1 = (num1*10 + l1->data)%1000000007;
+      l1 = l1->next;
+  }
+  while(l2!=nullptr){
+      num2 = (num2*10 + l2->data)%1000000007;
+      l2 = l2->next;
+  }
+  return (num1*num2)%1000000007;
+}
